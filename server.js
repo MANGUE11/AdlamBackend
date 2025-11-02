aa9f8dd68fca79c9b888d870c74fc4f6869bd5ba
SyllaMangue13 <manguemasysylla13@gmail.com>
2025-11-02 23:35:18 +0100

    modif server js

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const port = process.env.PORT || 8080
 // ----------------------------------------------------
 const allowedOrigins = [
   'https://adlam-frontend.vercel.app', // ton front en prod
+  'http://localhost:5173',
   'http://localhost:3000', // utile pour le dev local
   '*',
 ]
